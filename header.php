7d9d97e67df0e3332729c32e4fe5bb64fe2d6f7d
xcatliu <xcatliu@gmail.com>
2014-04-12 11:06:29 +0800

    init normalize.css

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>mangastream</title>
+        <link rel="stylesheet" href="<?php echo bloginfo('stylesheet_directory') . '/bower_components/normalize-css/normalize.css'; ?>">
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    </head>
+    <body>
+        <div class="header">
+            <div class="logo">
+                <h1>mangastream</h1>
+            </div>
+        </div>
+

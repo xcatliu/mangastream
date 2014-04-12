bd6be6428a83a9284b2578e82d5402ec6f441d64
xcatliu <xcatliu@gmail.com>
2014-04-12 15:52:48 +0800

    add header & gridism

--- header.php
+++ header.php
@@ -2,13 +2,47 @@
 <html>
     <head>
         <title>mangastream</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
         <link rel="stylesheet" href="<?php echo bloginfo('stylesheet_directory') . '/bower_components/normalize-css/normalize.css'; ?>">
+        <link rel="stylesheet" href="<?php echo bloginfo('stylesheet_directory') . '/bower_components/gridism/gridism.css'; ?>">
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     </head>
-    <body>
+    <body class="wrap wider">
         <div class="header">
-            <div class="logo">
-                <h1>mangastream</h1>
+            <div class="grid">
+                <div class="unit one-third">
+                    <a href="/">
+                        <div class="logo">
+                        </div>
+                    </a>
+                </div>
+                <div class="unit two-thirds">
+                    <div class="block_leader float-right">
+                    </div>
+                </div>
+            </div>
+            <div class="grid">
+                <div class="unit whole">
+                    <div class="nav">
+                        <ul class="nav-ul">
+                            <li class="nav-li">
+                                <a>Home</a>
+                            </li>
+                            <li class="nav-li">
+                                <a>Read Manga</a>
+                            </li>
+                            <li class="nav-li">
+                                <a>Recruitment</a>
+                            </li>
+                            <li class="nav-li">
+                                <a>Submit a Color Page</a>
+                            </li>
+                            <li class="nav-li">
+                                <a>Settings</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
 
